88000f557acda7022251880064c8fe405bdaf3b4
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-05-02 11:51:57 -0500

    Execises

---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Student {
+    //requirements for the Student class:
+    //
+    //id should be a long number used to represent a "unique user" in our application.
+    private long id;
+    //name is a String that holds the name of the student.
+    private String name;
+    //grades is an ArrayList that contains a list of Integer numbers.
+    private ArrayList<Integer> grades;
+
+}
